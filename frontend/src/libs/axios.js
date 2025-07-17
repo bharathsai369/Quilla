edf1fc2732f8df2f81b6d8f796779bc6a9d002b1
bharath <bharathsai257@gmail.com>
2025-07-18 00:20:51 +0530

    5

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api"
-    : import.meta.env.VITE_API_URL ?? "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+// : import.meta.env.VITE_API_URL ?? "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
