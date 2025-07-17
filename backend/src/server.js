1b25536bee88de4a5f9040a8d5a530d357ddb484
bharath <bharathsai257@gmail.com>
2025-07-18 00:12:17 +0530

    4

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || "http://localhost:5173";
+const __dirname = path.resolve();
 
 //middleware
-app.use(
-  cors({
-    origin: [ALLOWED_ORIGIN],
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      // origin: [ALLOWED_ORIGIN],
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -27,6 +32,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
