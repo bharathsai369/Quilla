7c94d43e2e6e7c009a73b2311dbcc1ef94d5c6bb
bharath <bharathsai257@gmail.com>
2025-07-18 13:39:55 +0530

    8

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
@@ -19,9 +19,17 @@ const PORT = process.env.PORT || 3000;
 // CORS setup
 app.use(
   cors({
-    origin: process.env.ALLOWED_ORIGIN || "*",
+    origin: ["http://localhost:5173"],
+    // origin: process.env.ALLOWED_ORIGIN || "http://localhost:5173" || "*",
   })
 );
+// if (process.env.NODE_ENV !== "production") {
+//   app.use(
+//     cors({
+//       origin: "http://localhost:5173",
+//     })
+//   );
+// }
 
 // Middleware
 app.use(express.json());
@@ -31,12 +39,12 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 // Connect DB and Start Server
 connectDB().then(() => {
